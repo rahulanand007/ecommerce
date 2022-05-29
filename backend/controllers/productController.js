710e3cef53b5ca8ea95f296a52782fe925a03134
rahulanand007 <rahul03061998@gmail.com>
2022-05-29 07:04:03 +0530

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,84 @@
+const Product = require('../models/productModel')
+
+
+
+//Create Product -- Admin
+exports.createProduct = async(req,res,next)=>{
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+
+//Get all Products 
+exports.getAllProducts = async (req,res)=>{
+    const product = await Product.find()
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+//Get single product
+exports.getProductDetails = async (req,res)=>{
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+//Update Product -- Admin
+exports.updateProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new:true, 
+        runValidators:true, 
+        useFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+//Delete Product -- Admin
+
+exports.deleteProduct = async (req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"product delete successfully"
+    })
+}
+
+
